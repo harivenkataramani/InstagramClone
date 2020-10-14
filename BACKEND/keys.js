e85175e1a3937fbc797a010a0c0e9a8cffef22e2
harivenkataramani <venkata.hari18@gmail.com>
2020-10-14 16:26:17 +0530

    deployment changes

--- BACKEND/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb://Harivenkataramani:Wvg1DW87C6HHNMKo@cluster0-shard-00-00.sqp70.mongodb.net:27017,cluster0-shard-00-01.sqp70.mongodb.net:27017,cluster0-shard-00-02.sqp70.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-ozgaxp-shard-0&authSource=admin&retryWrites=true&w=majority",
-
-  // "mongodb+srv://Harivenkataramani:Wvg1DW87C6HHNMKo@cluster0.sqp70.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  JWT_SECRET:
-    "this_is_the_top_most_secret_i_wish to have_for_my_app_lets_do_thisss",
-};
